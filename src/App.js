652246bc605db9caca6cd351e74527eab2fc50f5
Ilya <ilyha13@gmail.com>
2018-10-27 12:22:01 +0500

    123231

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'antd/es/grid/style/index.js'
+
 class App extends Component {
+	state = {
+		values: [1, 2, 3],
+	};
+	
+	newvalue = () => {
+		let values = this.state.values;
+		values.push(1)
+		this.setState({values})
+	}
+	
+	
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+		<div className="header">
+			<div className="Journeys">Journeys</div><br/>
+		</div>
+		<div className="text">Team vision: Core Values</div>
+		<div className="box"></div>
+		<div className="container">
+			{this.state.values.map((e,i) => (<div key={i} className="value">{e}</div>))}
+			<div onClick={this.newvalue} className="addvalue"></div>
+		</div>
       </div>
     );
   }
